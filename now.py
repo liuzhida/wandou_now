0417bf2deaaaaa26132caf98d594ebb5d54c9729
zhida <zhida@wandoujia.com>
2014-02-06 17:46:46 +0800

    dianping api

--- now.py
+++ now.py
@@ -7,111 +7,41 @@ import tornado.options
 import tornado.auth
 from tornado.options import define, options
 
-from ldap_auth import Auth
 import json
+import redis
+
+c = redis.Redis(host='127.0.0.1', port=6379, db=0)
 
 
-class BaseHandler(tornado.web.RequestHandler):
-
-    def get_current_user(self):
-        return self.get_secure_cookie("user")
-
-
-class LogoutHandler(BaseHandler):
+class TestHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.clear_cookie("user")
-        self.redirect("/")
+        shoplist = '<a href="dianping://shopinfo?id=4175436">test</>'
+        return self.finish(shoplist)
 
 
-class LoginHandler(tornado.web.RequestHandler):
+class ShopHandler(tornado.web.RequestHandler):
 
     def get(self):
-        return self.render('login.html')
-
-    def post(self):
-        mail = self.get_argument('mail')
-        if mail.endswith("@wandoujia.com"):
-            mail = mail.strip("@wandoujia.com")
-        passwd = self.get_argument("passwd")
-        if Auth(mail, passwd):
-            user = dict()
-            user['email'] = mail + "@wandoujia.com"
-            self.set_secure_cookie("user", json.dumps(user))
-            self.redirect("/")
-        else:
-            return self.render('login_failed.html')
-
-
-class IndexHandler(BaseHandler):
-
-    @tornado.web.authenticated
-    def get(self):
-        if not self.current_user:
-            raise tornado.web.HTTPError(403)
-            return
-        self.user = json.loads(self.current_user)
-        email = self.user["email"]
-        name = email.split("@")[0]
-        self.render('index.html', name=name)
-
-    @tornado.web.authenticated
-    def post(self):
-        if not self.current_user:
-            raise tornado.web.HTTPError(403)
-            return
-        self.user = json.loads(self.current_user)
-        email = self.user["email"]
-        name = email.split("@")[0]
-        self.render('index.html', name=name)
-
-
-class ManagerHandler(BaseHandler):
-
-    @tornado.web.authenticated
-    def get(self):
-        if not self.current_user:
-            raise tornado.web.HTTPError(403)
-            return
-        self.user = json.loads(self.current_user)
-        self.render('manager.html')
-
-
-class AdminHandler(BaseHandler):
-
-    @tornado.web.authenticated
-    def get(self):
-        if not self.current_user:
-            raise tornado.web.HTTPError(403)
-            return
-        self.user = json.loads(self.current_user)
-        self.render('admin.html')
-
-
-class ReviewHandler(BaseHandler):
-
-    @tornado.web.authenticated
-    def get(self):
-        if not self.current_user:
-            raise tornado.web.HTTPError(403)
-            return
-        self.user = json.loads(self.current_user)
-        self.render('admin.html')
+        idlist = c.lrange("shoplist", 0, -1)
+        shoplist = list()
+        for id in idlist:
+            shop = c.hgetall(id)
+            shop['tags'] = eval(shop['tags'])
+            shoplist.append(shop)
+        shoplist = json.dumps(shoplist)
+        return self.finish(shoplist)
 
 
 def main():
     define("port", default=8089, help="run on the given port", type=int)
     settings = {"debug": True, "template_path": "templates",
-                "static_path": "static", "login_url": "/login",
+                "static_path": "static",
                 "cookie_secret": "z1DAVh+WTvyqpWGmOtJCQLETQYUznEuYskSF06To="}
     tornado.options.parse_command_line()
     application = tornado.web.Application([
-        (r"/", IndexHandler),
-        (r"/login", LoginHandler),
-        (r"/logout", LogoutHandler),
-        (r"/manager", ManagerHandler),
-        (r"/admin", AdminHandler),
-        (r"/review", ReviewHandler),
+        (r"/shop", ShopHandler),
+        (r"/test", TestHandler),
 
     ], **settings)
     http_server = tornado.httpserver.HTTPServer(application)
